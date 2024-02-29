e16580fbc4947022fe85d0e4461bc7ef233ca8ca
Manny6902 <mpylla@purdue.edu>
2024-02-29 18:26:32 -0500

    navbar update (bugged)

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 import "../styles/Navbar.css";
-//import ReorderIcon from '@mui/material-ui/icons/Reorder'
+import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
-  return (
-    <div className='navbar'>
-        <div className='toggleButton'>
-            <button></button>
+    const [expandNavbar, setExpandNavbar] = useState(false);
+
+  
+    return (
+        <div className='navbar' id={expandNavbar ? "open" : "close"}>
+            <div className='toggleButton'>
+                <button onClick={() => {setExpandNavbar((prev) => !prev)}}>
+                    <ReorderIcon></ReorderIcon>
+                </button>
+            </div>
+            <div className='links'>
+                <Link to="/">Home</Link>
+                <Link to="/projects">Projects</Link>
+            </div>
         </div>
-        <div className='links'>
-            <Link to="/">Home</Link>
-            <Link to="/projects">Projects</Link>
-        </div>
-    </div>
-  )
+    );
 }
 
 export default Navbar
